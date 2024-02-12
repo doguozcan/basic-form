9555f578a02935a5c619a6ec983bce3e8a805ff5
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-02-12 09:04:07 +0300

    Basic Form Implemented

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Form from './Form'
+
+export default function App() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center bg-slate-100 font-lato">
+      <Form />
+    </div>
+  )
+}
---
+++ src/Form.js
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+
+export default function Form() {
+  const [inputValue, setInputValue] = useState({
+    firstName: '',
+    lastName: '',
+  })
+
+  const [submitted, setSubmitted] = useState(false)
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setInputValue({ ...inputValue, [name]: value })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setSubmitted(true)
+
+    if (inputValue.firstName && inputValue.lastName) {
+      alert(
+        `Form successfully submitted with the following values: ${inputValue.firstName} ${inputValue.lastName}`
+      )
+      setInputValue({ firstName: '', lastName: '' })
+      setSubmitted(false)
+    }
+  }
+
+  const inputStyle = 'border border-2 rounded-md p-2 shadow-sm text-center'
+
+  return (
+    <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+      <input
+        className={inputStyle}
+        name="firstName"
+        value={inputValue.firstName}
+        placeholder="First Name"
+        onChange={handleChange}
+      ></input>
+      {submitted && !inputValue.firstName ? (
+        <p className="text-red-500">Please enter your first name</p>
+      ) : (
+        ''
+      )}
+      <input
+        className={inputStyle}
+        name="lastName"
+        value={inputValue.lastName}
+        placeholder="Last Name"
+        onChange={handleChange}
+      ></input>
+      {submitted && !inputValue.lastName ? (
+        <p className="text-red-500">Please enter your last name</p>
+      ) : (
+        ''
+      )}
+      <button className="bg-slate-200 rounded-md p-2 shadow-sm" type="submit">
+        Submit
+      </button>
+    </form>
+  )
+}
